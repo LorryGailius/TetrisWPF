bcf40c33e346ea5d0b4221fd8bfc14f54814e93e
Laurynas <lauris.gailius@gmail.com>
2023-09-23 18:25:42 +0300

    Created abstract Block class

---
+++ TetrisWPF/Blocks/Block.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace TetrisWPF.Blocks
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationIndex;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> GetTiles()
+        {
+            foreach (Position p in Tiles[rotationIndex])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        public void RotateClockwise()
+        {
+            rotationIndex = (rotationIndex + 1) % Tiles.Length;
+        }
+
+        public void RotateCounterClockwise()
+        {
+            if (rotationIndex == 0)
+            {
+                rotationIndex = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationIndex--;
+            }
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset = new Position(offset.Row + rows, offset.Column + columns);
+        }
+
+        public void Reset()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+            rotationIndex = 0;
+        }
+    }
+}
